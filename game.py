ae23bcc38525b9b51201a2793320b31377f55475
jesteartyste <108098593+jesteartyste@users.noreply.github.com>
2022-09-27 18:21:21 +0200

    Delete game.py

--- game.py
+++
@@ -1,6 +0,0 @@
-from quiz_game import Quiz_Game
-
-# Start Game
-
-name = input("What is your name?:  ")
-print(f"Hey {name} ! Welcome in Quiz Game! For every correct answer, you get 1 point. Let's start the game!")
